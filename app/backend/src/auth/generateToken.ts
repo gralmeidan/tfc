badf332eb449d1a998384c03ab62c156c17a63e7
gralmeidan <gralmeidan@gmail.com>
2022-10-24 16:23:54 -0400

    Req3: Added function to generate JWT tokens

---
+++ app/backend/src/auth/generateToken.ts
@@ -0,0 +1,11 @@
+import * as jwt from 'jsonwebtoken';
+import User from '../types/user.type';
+
+export default function generateToken(user: User) {
+  const secret = process.env.JWT_SECRET || 'secret';
+
+  return jwt.sign(user, secret, {
+    algorithm: 'HS256',
+    expiresIn: '7d',
+  });
+}
--- app/backend/src/tests/unit/auth/generateToken.test.ts
+++ app/backend/src/tests/unit/auth/generateToken.test.ts
@@ -5,12 +5,12 @@ import { validAdmin } from '../../mocks/user.mock';
 import { expect } from 'chai';
 
 describe('Unit tests for auth/generateToken', () => {
-  it('Should return a token', () => {
+  it('Should return a token', async () => {
     const fakeToken =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
     sinon.stub(jwt, 'sign').resolves(fakeToken);
 
-    const token = generateToken(validAdmin.hiddenPassword);
+    const token = await generateToken(validAdmin.hiddenPassword);
 
     expect(token).to.equal(fakeToken);
   });
