10cbcabf9f986420964ec7878cb6200380d0c9c4
gralmeidan <gralmeidan@gmail.com>
2022-10-27 17:34:23 -0400

    Req25: Updated match.schema to disallow repeating teams and changed verifyToken's error message

--- app/backend/src/auth/verifyToken.middleware.ts
+++ app/backend/src/auth/verifyToken.middleware.ts
@@ -3,7 +3,11 @@ import * as jwt from 'jsonwebtoken';
 import RestError from '../error/RestError';
 import User from '../types/user.type';
 
-export default async function verifyToken(req: Request, _res: Response, next: NextFunction) {
+export default async function verifyToken(
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) {
   try {
     const secret = process.env.JWT_SECRET || 'secret';
     const token = req.headers.authorization;
@@ -17,6 +21,6 @@ export default async function verifyToken(req: Request, _res: Response, next: Ne
     req.user = payload as User;
     next();
   } catch (err) {
-    throw new RestError(401, (err as Error).message);
+    throw new RestError(401, 'Token must be a valid token');
   }
 }
--- app/backend/src/services/joi/match.schemas.ts
+++ app/backend/src/services/joi/match.schemas.ts
@@ -12,17 +12,22 @@ export const querySchema = Joi.object({
   inProgress: Joi.boolean().optional(),
 });
 
+const awayTeam = id.disallow(Joi.ref('homeTeam')).messages({
+  'any.invalid':
+    'It is not possible to create a match with two equal teams',
+});
+
 export const newMatchSchema = Joi.object({
   homeTeam: id.required(),
   homeTeamGoals: goals.required(),
-  awayTeam: id.required(),
+  awayTeam: awayTeam.required(),
   awayTeamGoals: goals.required(),
 }).required();
 
 export const updateSchema = Joi.object({
   homeTeam: id.optional(),
   homeTeamGoals: goals.optional(),
-  awayTeam: id.optional(),
+  awayTeam: awayTeam.optional(),
   awayTeamGoals: goals.optional(),
   inProgress: Joi.boolean().optional(),
 }).min(1);
